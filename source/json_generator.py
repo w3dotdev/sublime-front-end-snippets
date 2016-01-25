0cbd2962fcaa7810e4dc554708550a2c58c7a551
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-01-25 12:09:17 -0200

    update: filename

--- source/json_generator.py
+++ source/json_generator.py
@@ -21,15 +21,18 @@ def getDir(path, ext):
       matches.append(os.path.join(root, filename))
   return matches
 
+def getJson():
+  result = []
+  for count, file in enumerate(getDir('../snippets/', '*.sublime-snippet'), start=1):
+    line        = open(file)
+    content     = line.read()
+    trigger     = content.split('<tabTrigger>')[1].split('</tabTrigger>')[0]
+    description = content.split('<description>')[1].split('</description>')[0]
 
-result = []
-for count, file in enumerate(getDir('../snippets/', '*.sublime-snippet'), start=1):
-  line        = open(file)
-  content     = line.read()
-  trigger     = content.split('<tabTrigger>')[1].split('</tabTrigger>')[0]
-  description = content.split('<description>')[1].split('</description>')[0]
+    result.append({'id':count, 'trigger':trigger, 'description':description})
 
-  result.append({'id':count, 'trigger':trigger, 'description':description})
+  return result
 
-writeJson(result, "../snippets.json")
+
+writeJson(getJson(), "../snippets.json")
 
